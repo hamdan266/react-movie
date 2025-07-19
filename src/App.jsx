a092a1d385d8d919bc1e523b02d843c43ac82666
Hamdan junaedi <49368693+hamdan266@users.noreply.github.com>
2025-07-20 04:57:43 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ const App = () => {
           <img src="./hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
-            Without the Hassle
+            Without the Hasslee
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
